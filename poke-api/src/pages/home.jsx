44fdfaa82350033838bd476dbaea5fbd7e242129
jeanroths <jean.machado@sou.inteli.edu.br>
2023-08-22 16:16:43 -0300

    alterações front

--- poke-api/src/pages/home.jsx
+++ poke-api/src/pages/home.jsx
@@ -1,31 +1,39 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-function SearchBar() {
-  const [searchQuery, setSearchQuery] = useState('');
+function Home() {
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonData, setPokemonData] = useState(null);
 
-  const handleSearchChange = (e) => {
-    setSearchQuery(e.target.value);
-  };
-
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    // Aqui você pode adicionar a lógica para lidar com a submissão da pesquisa
-    console.log('Search submitted:', searchQuery);
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/pokemon/${pokemonName}`);
+      setPokemonData(response.data);
+    } catch (error) {
+      console.error("Error fetching Pokémon data:", error);
+      setPokemonData(null);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={handleSearchSubmit}>
-        <input
-          type="text"
-          placeholder="Search Pokémon"
-          value={searchQuery}
-          onChange={handleSearchChange}
-        />
-        <button type="submit">Search</button>
-      </form>
+      <h1>Pokémon Search</h1>
+      <input
+        type="text"
+        placeholder="Enter Pokémon name"
+        value={pokemonName}
+        onChange={(e) => setPokemonName(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      {pokemonData && (
+        <div>
+          <h2>{pokemonData.name}</h2>
+          {/* Render other details about the Pokémon */}
+        </div>
+      )}
     </div>
   );
 }
 
-export default SearchBar;
+export default Home;
