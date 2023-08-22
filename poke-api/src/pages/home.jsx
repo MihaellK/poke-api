50fa8db410e116d16143e8e0668afc67dfb12075
jeanroths <jean.machado@sou.inteli.edu.br>
2023-08-22 15:40:08 -0300

    home search

--- poke-api/src/pages/home.jsx
+++ poke-api/src/pages/home.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
 
-const Home = () => {
-    return(
-        <></>
-    )
+function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    // Aqui você pode adicionar a lógica para lidar com a submissão da pesquisa
+    console.log('Search submitted:', searchQuery);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          placeholder="Search Pokémon"
+          value={searchQuery}
+          onChange={handleSearchChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
 
-export default Home;
+export default SearchBar;
