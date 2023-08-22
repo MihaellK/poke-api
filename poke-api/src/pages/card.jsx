5ed6561d91fce245600ae4e548a40e7624a12a1d
MihaellK <MihaellK@users.noreply.github.com>
2023-08-22 15:13:43 -0300

    .

---
+++ poke-api/src/pages/card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div className="card">
+            <h1>Pokename</h1>
+            <img src=""></img>
+        </div>
+    )
+}
+
+export default Card;
